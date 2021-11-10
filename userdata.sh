dcb68a1996f051347b254d5e137298f02382ba43
Joseph Ayodele <fisayoayodele01@gmail.com>
2021-11-09 23:29:47 -0600

    iam role added to cloudformation

--- userdata.sh
+++ userdata.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 yum -y update
-yum install git -y
-curl -O https://bootstrap.pypa.io/get-pip.py
-python3 get-pip.py --user
-rm get-pip.py
-git clone https://github.com/QuantumQuackDoctor/data-producers.git
+yum -y install git 
+python -m ensurepip --upgrade
+cd /home/ec2-user
+su ec2-user -c "git clone https://github.com/QuantumQuackDoctor/data-producers.git"
 cd data-producers/ && pip3 install -r requirements.txt
