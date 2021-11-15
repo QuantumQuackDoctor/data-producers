01d3e24e87ad7a39a728f75c3105c21af6716c50
Olufisayo Joseph Ayodele <fisayoayodele01@gmail.com>
2021-11-14 22:39:16 -0600

    testing stuff

--- food_order_producer/generate_orders.py
+++ food_order_producer/generate_orders.py
@@ -114,7 +114,7 @@ def add_order_to_db():
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO, format='%(asctime)s %(name)s %(levelname)s:%(message)s')
     logger = logging.getLogger(__name__)
-    for i in range(1):
+    for i in range(100):
         try:
             add_order_to_db()
         except Exception:
